ab38533273e2d1dd4ef89ffff0bf9be38eead442
yoyogold-a11 <tmrfack@gmail.com>
2021-07-26 23:02:25 -0500

    adding files

--- js/main.js
+++ js/main.js
@@ -119,21 +119,6 @@
         }
     });
 
-    // acortion
 
-    const accordionItemHeaders = document.querySelectorAll(".blog .blog-title");
-    accordionItemHeaders.forEach(accordionItemHeader => {
-        accordionItemHeader.addEventListener("click", event => {
-            accordionItemHeader.classList.toggle("active");
-            const accordionItemBody = accordionItemHeader.nextElementSibling;
-            if(accordionItemHeader.classList.contains("active")) {
-              accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-            }
-            else {
-              accordionItemBody.style.maxHeight = 0;
-            }
-            
-          });
-        });
 })(jQuery);
 
