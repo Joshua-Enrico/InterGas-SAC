a9504d7617405845820f83ca38273797d82b8b7c
yoyogold-a11 <tmrfack@gmail.com>
2021-08-05 18:06:32 -0500

    several cheanges

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@
         return false;
     });
     
-    
+
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 90) {
@@ -75,25 +75,7 @@
 
 
     // Testimonial Slider
-    $('.testimonial-slider').slick({
-        infinite: true,
-        autoplay: true,
-        arrows: false,
-        dots: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        asNavFor: '.testimonial-slider-nav'
-    });
-    $('.testimonial-slider-nav').slick({
-        arrows: true,
-        dots: false,
-        focusOnSelect: true,
-        centerMode: true,
-        centerPadding: '22px',
-        slidesToShow: 3,
-        asNavFor: '.testimonial-slider'
-    });
-    $('.testimonial .slider-nav').css({"position": "relative", "height": "160px"});
+
     
     
     // Blogs carousel
