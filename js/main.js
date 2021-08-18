d58782828a0119414054c0af1dcd5d78d88b418b
yoyogold-a11 <tmrfack@gmail.com>
2021-08-18 03:38:14 -0500

    testing

--- js/main.js
+++ js/main.js
@@ -98,7 +98,24 @@
             }
         }
     });
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "102859152094991");
+    chatbox.setAttribute("attribution", "biz_inbox");
 
+    window.fbAsyncInit = function () {
+        FB.init({
+            xfbml: true,
+            version: 'v11.0'
+        });
+    };
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
 })(jQuery);
 
